02d30bd00048b0508413e3387322a37bd39c7a9c
Benedict Yong <103986225+askavania@users.noreply.github.com>
2023-03-01 11:49:45 +0800

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -33,7 +33,7 @@ def predict(model, opencv_Image):
 
     # Map the predicted index to the class name
     class_names = ['No2 Deficiency observed - Class 1:  \n Apply N-Fertilizer immediately', 
-                   'N02 Deficiency observed - \n Apply N-Fertilizer soon', 
+                   'N02 Deficiency observed - Class 2: \n Apply N-Fertilizer soon', 
                    'Ideal range - Class 3: \n Do not apply N-Fertilizer and continue to monitor closely', 
                    'Ideal range - Class 4: \n Do not apply N-Fertilizer and continue to monitor']
     class_name = class_names[class_index]
