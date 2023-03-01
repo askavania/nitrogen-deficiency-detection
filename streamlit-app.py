036a8125281d9369019029cceed0863b014d7ee1
Benedict Yong <103986225+askavania@users.noreply.github.com>
2023-03-01 12:28:21 +0800

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -56,7 +56,7 @@ st.markdown("""
 st.title("Nitrogen Deficiency for Rice Crop Prediction App")
 st.write("Upload or take a photo of a rice leaf to see if it has nitrogen deficiency or not!")
 
-tab1, tab2, tab3 = st.tabs(["Upload Image", "Capture Image", "Use Case Test Images"])
+tab1, tab2 = st.tabs(["Upload Image", "Capture Image"])
 
 with tab1:
     test_image = st.file_uploader('Image', type=['jpg', 'png','jpeg', 'jfif'] )
@@ -83,67 +83,3 @@ with tab2:
         prediction = predict(my_model, test_image_decoded)
         st.subheader('Predicted Class')
         st.write(prediction)
-
-
-        # Define a dictionary of preset images and their corresponding labels
-preset_images = {
-    'Class 1 sample 1': 'https://github.com/askavania/nitrogen-deficiency-detection/blob/main/Data/NitrogenDeficiencyImage/RealWorldTest/googleimage1-class1.jpg',
-    'Class 1 sample 2': 'https://github.com/askavania/nitrogen-deficiency-detection/blob/main/Data/NitrogenDeficiencyImage/RealWorldTest/googleimage2.jpg',
-    'Class 1 sample 3': 'https://github.com/askavania/nitrogen-deficiency-detection/blob/main/Data/NitrogenDeficiencyImage/RealWorldTest/googleimage4.jpg',
-    'Class 1 sample 4': 'https://github.com/askavania/nitrogen-deficiency-detection/blob/main/Data/NitrogenDeficiencyImage/RealWorldTest/googleimage6.jpg',
-    'Class 1 sample 3': 'https://github.com/askavania/nitrogen-deficiency-detection/blob/main/Data/NitrogenDeficiencyImage/RealWorldTest/googleimage4.jpg',
-    'Class 3 sample 1': 'https://github.com/askavania/nitrogen-deficiency-detection/blob/main/Data/NitrogenDeficiencyImage/RealWorldTest/googleimage7.jpg',
-    'Class 3 sample 2': 'https://github.com/askavania/nitrogen-deficiency-detection/blob/main/Data/NitrogenDeficiencyImage/RealWorldTest/googleimage8.jpg',
-    'Class 4 sample 1': 'https://github.com/askavania/nitrogen-deficiency-detection/blob/main/Data/NitrogenDeficiencyImage/RealWorldTest/unknown4.jpg',
-}
-
-# Define a new tab for the preset images
-# with tab3:
-#     st.title('Preset Images')
-#     for label, image_url in preset_images.items():
-#         col1, col2 = st.columns(2)
-#         with col1:
-#             st.subheader(label.title())
-#             st.image(image_url, use_column_width=True)
-#         with col2:
-#             st.write('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis euismod ut nulla ac efficitur. Aenean rhoncus dolor quis erat congue bibendum.')
-
-            
-# Define the images and their corresponding labels
-image_paths = preset_images.items()
-labels = preset_images.keys()
-
-# Define a function to make predictions for preset images
-def predict_preset_images(model, image_paths):
-    predictions = []
-    for path in image_paths:
-        image = cv2.imread(path)
-        prediction = predict(model, image)
-        predictions.append(prediction)
-    return predictions
-
-# Create a new tab for preset images
-with tab3:
-    st.markdown('## Preset Images')
-    st.image(Image.open('https://github.com/askavania/nitrogen-deficiency-detection/blob/main/Data/NitrogenDeficiencyImage/RealWorldTest/googleimage1-class1.jpg'))
-    selected_image = st.selectbox('Select an image:', options=image_paths)
-    col1, col2 = st.columns(2)
-#     if st.button('Predict'):
-#         image = cv2.imread(st.image(selected_image))
-#         prediction = predict(my_model, image)
-#         st.write('Prediction:', prediction)
-#         col1.subheader('Uploaded Image')
-#         col1.image(image, channels = "BGR")
-#         prediction = predict(my_model, image)
-#         col2.subheader('Predicted Class')
-#         col2.write(prediction)
-
-# Create another tab for multiple preset images
-#with st.sidebar:
-#     st.markdown('## Multiple Preset Images')
-#     st.write('This tab will predict the labels of multiple preset images.')
-#     if st.button('Predict All'):
-#         predictions = predict_preset_images(my_model, image_paths)
-#         st.write('Predictions:')
-#         for i, label in enumerate(labels):
-#             st.write(f'{label}: {predictions[i]}')
