7d5a0c68a7f21028a137787d59720703a3b0bdec
Benedict Yong <103986225+askavania@users.noreply.github.com>
2025-05-13 01:40:19 +0800

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -23,7 +23,7 @@ def load_model(path="EfficientNet_B4NO2Model.pt"):
         nn.Linear(512, 4)
     )
     # load only the state dict
-    sd = torch.load(path, map_location=device)
+    sd = torch.load(path, map_location=device, weights_only=False)
     model.load_state_dict(sd)
     return model.to(device).eval()
 
