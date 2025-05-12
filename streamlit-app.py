39a384c9d359c3cd60078679680debeb6d7f0620
Benedict Yong <103986225+askavania@users.noreply.github.com>
2025-05-13 01:03:05 +0800

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -11,6 +11,8 @@ import cv2
 # --- allowlist EfficientNet for safe unpickling under PyTorch ≥2.6 ---
 torch.serialization.add_safe_globals([efficientnet.EfficientNet, container.Sequential])
 
+torch.serialization.add_safe_globals([torchvision.ops.misc.Conv2dNormActivation])
+
 # device setup
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
